023a586809b4c5d890a1af826cb7c7611ec2a339
baudmap <baudmap@4e451f21-b44b-0410-a7af-17a7e65364fd>
2008-06-14 15:02:11 +0000

    Get privileged JS vars right

--- trunk/application/chrome/content/AliwalGeocoder/lib.DomManager.js
+++ trunk/application/chrome/content/AliwalGeocoder/lib.DomManager.js
@@ -1,8 +1,223 @@
 /* ****************************************************************************************************************************** */
 function DomManager(){
 	/* A namespace for DOM functionality */
+	
+	var that = this; // http://javascript.crockford.com/private.html
+	
 	this.tagsetnum=0;
+	this.pinLabels = [];
+	
+	this.drawMarkers = function( pVisibleMarkers, pHiddenMarkers, pBounds ){
+		/* Takes a hash of marker objects and puts them on the map.
+		 * Privileged method.
+		 */
+		var fils = that.getFilterSelection();
+		var visicount = 0;
+		this.warningPinCeiling(false);
+		
+		for( var key in pVisibleMarkers ){	
+			if( that.inBounds(pBounds, pVisibleMarkers[key].YGeoPoint.Lat, pVisibleMarkers[key].YGeoPoint.Lon) 
+			  && markerMgr.unfilteredMarker(pVisibleMarkers[key], fils) ){
+				// leave already visible,inBounds&unfiltered alone
+				visicount++;
+			} else {
+				// Remove visible&(filtered or out of bounds ) to hidden
+				//jsdump('Moving from visible to hidden: ' + pVisibleMarkers[key].id );
+				pHiddenMarkers[key] = pVisibleMarkers[key];
+				map.removeOverlay(pVisibleMarkers[key]);
+				delete pVisibleMarkers[key];
+			}
+		}
+		try{	
+			for( var key in pHiddenMarkers ){
+				if( that.inBounds(pBounds, pHiddenMarkers[key].YGeoPoint.Lat, pHiddenMarkers[key].YGeoPoint.Lon) ){
+					if( markerMgr.unfilteredMarker(pHiddenMarkers[key], fils) ){
+						if ( visicount < 200 ){
+							//jsdump('Moving from hidden to visible: ' + pHiddenMarkers[key].id );
+							visicount++;
+							pVisibleMarkers[key] = pHiddenMarkers[key];
+							map.addOverlay(pVisibleMarkers[key]);
+							delete pHiddenMarkers[key];	
+						} else {
+							throw 'Map density ceiling';
+					 	}
+					}
+				} else if( pHiddenMarkers[key].YGeoPoint.Lat === 0 && pHiddenMarkers[key].YGeoPoint.Lon === 0 ){
+					// The marker has been created but not yet geocoded. Add it to the map and 
+					// leave it alone until it's been geocoded.
+					visicount++;
+					pVisibleMarkers[key] = pHiddenMarkers[key];
+					map.addOverlay(pVisibleMarkers[key]);
+					delete pHiddenMarkers[key];	
+				}
+			}
+		} catch(e){
+			this.warningPinCeiling(true);
+		}
+		var totalcount = 0;
+		for(var m in pVisibleMarkers){
+			totalcount++;
+		}
+		for(var m in pHiddenMarkers){
+			totalcount++;
+		}
+		$('#feedback_pincounts').text(visicount + ' / ' + totalcount);
+		$('#sel_change_pin_label').trigger( 'change');
+	}
+	
+	this.drawInitMarkers = function( pPoll, pCallback){
+		/* If pPoll then wait until xscopeNS.flags.loadingData is finished.
+		 * Privileged method.
+		 */
+		if( pPoll){
+			if(xscopeNS.flags.loadingData){
+				setTimeout( that.drawInitMarkers,555, true, pCallback );
+			} else {
+				setTimeout( that.drawInitMarkers,1, false, pCallback );
+			}
+		} else {
+			dataMgr.emptyObj( xscopeNS.pointMarkers );
+			dataMgr.emptyObj( xscopeNS.hiddenMarkers );
+			try{
+				markerMgr.createDomPointMarkers(xscopeNS.KML, xscopeNS.hiddenMarkers );
+			} catch(e){
+				jsdump('Exception:\n'+e);
+			}
+			pCallback.call(that);
+		}
+	}
+	
+	this.drawInitPointlessMarkers = function( pPoll, pCallback ){
+		/* Markers without coordinates are plonked onto the map and left there until
+		 * their geocoding comes back.
+		 * Privileged method.
+		 */
+		if( pPoll){
+			if(xscopeNS.flags.loadingData){
+				setTimeout( that.drawInitPointlessMarkers,555, true, pCallback );
+			} else {
+				setTimeout( that.drawInitPointlessMarkers,1, false, pCallback );
+			}
+		} else {
+			dataMgr.emptyObj( xscopeNS.geoMarkers );
+			dataMgr.emptyObj( xscopeNS.errorMarkers );
+			try{
+				markerMgr.createDomGeocodeMarkers(xscopeNS.KML, xscopeNS.geoMarkers );
+			} catch(e){
+				jsdump('Exception:\n'+e);
+			}
+			$.each(xscopeNS.geoMarkers, function(key, mkr){
+				map.addOverlay(xscopeNS.geoMarkers[key]);
+			});
+			pCallback.call(that);
+		}
+	}
+	
+	this.drawLabelSelector = function( pDivId ){
+		/* Args: The string id of the div to draw the label selector in 
+		 * Privileged method.
+		 */
+		var dd = $('#' + pDivId);
+		dd.empty();
+		var sel = '<SELECT id="sel_change_pin_label">';
+		$.each( that.pinLabels, function(i,val){
+			sel += '<OPTION>'+val+'</OPTION>\n';
+		});
+		sel += '</SELECT>';
+		if( that.pinLabels.length > 0){
+			dd.append( sel );
+		} else {
+			dd.append('<div class="warn">No data</div>');
+			jsdump('No pin labels found in data');
+		}
+	}
+	
+	this.drawPopupSelector= function( pDivId ){
+		/* Args: The string id of the div to draw the selector in 
+		 * Privileged method.
+		 */
+		var dd = $('#' + pDivId);
+		dd.empty();
+		var html = '';	
+		$.each( that.pinLabels, function(i, val){
+			html += '<UL id="ul_popup_selector">';
+			html += '<LI><input class="cb_popup_selector"  type="checkbox" checked="checked" value="'+val+'">'+val+'</LI>';
+			html += '</UL> \n';
+		});
+		if( that.pinLabels.length > 0){
+			dd.append( html );
+		} else {
+			dd.append('<div class="warn">No data</div>');
+		}
+	}
+	
+	this.getPopupSelection = function( ){
+		/* returns an array of members that should be shown in popups.
+		 * Used to get this from the GUI, now just returns all the columns
+		 * Privileged method.
+		 */
+		// New version
+		//return that.pinLabels;
+		
+		// Old version below, just in case
+		var ret = new Array();
+		$('.cb_popup_selector:checked').each(function(i,v){
+			ret.push( $(v).val() );
+		});
+		return ret;
+	}
+	
+	this.drawControls = function( pPoll){
+		/* Draws the GUI controls.
+		 * Privileged Method
+		 */
+		if( pPoll){
+			if(xscopeNS.flags.loadingData){
+				setTimeout( domMgr.drawControls,555, true );
+			} else {
+				setTimeout( domMgr.drawControls,1, false );
+			}
+		} else {
+			// Filtering stuff
+			var pinTagSets = dataMgr.domTagSetCensus( xscopeNS.KML );
+				
+			var html = '<table id="tbl_filters"></table>';
+			$("#div_filters").empty();
+			$("#div_filters").append(html);
+		
+		 	var filcnt = 0;
+			$.each( pinTagSets, function(tagset, tags){
+				html = '<tr><td id="divFilter_' + filcnt + '"></td></tr>';
+				$('#tbl_filters').append(html);
+				domMgr.drawTagsetFilter('divFilter_' + filcnt, tagset, pinTagSets );
+				filcnt++;
+			});
+			
+			// Attach an event handler to the filter stuff
+			$('.tagset_filter').bind('change', function(e){
+				domMgr.drawMarkers( xscopeNS.pointMarkers, xscopeNS.hiddenMarkers, map.getBoundsLatLon() );
+			});
+			
+			// Pin label stuff
+			var pinItems = dataMgr.domDataCensus( xscopeNS.KML );	
+			that.pinLabels = [];
+			for (var pi in pinItems){
+				that.pinLabels.push(pi);
+			}
+			domMgr.drawLabelSelector('pin_label_selector' );
+			$('#sel_change_pin_label').bind( 'change', function(e){
+				markerMgr.setPinLabels( xscopeNS.pointMarkers, this.value );
+			});
+		
+			// Popup Selectors	
+			// Switched off. Unnecessary visual clutter.
+			domMgr.drawPopupSelector('pin_popup_switches' );	
+		}
+	}
+
 }
+
+
 DomManager.prototype.warningPinCeiling = function( pSet ){
 	if(pSet){
 		if(xscopeNS.flags.warnPinCountError){
@@ -48,110 +263,8 @@ DomManager.prototype.inBounds = function( pBounds, pLat, pLon ){
 	}
 	return false;
 }
-DomManager.prototype.drawMarkers = function( pVisibleMarkers, pHiddenMarkers, pBounds ){
-	/* Takes a hash of marker objects and puts them on the map.
-	 */
-	var that = this;
-	var fils = that.getFilterSelection();
-	var visicount = 0;
-	this.warningPinCeiling(false);
-	
-	for( var key in pVisibleMarkers ){	
-		if( that.inBounds(pBounds, pVisibleMarkers[key].YGeoPoint.Lat, pVisibleMarkers[key].YGeoPoint.Lon) 
-		  && markerMgr.unfilteredMarker(pVisibleMarkers[key], fils) ){
-			// leave already visible,inBounds&unfiltered alone
-			visicount++;
-		} else {
-			// Remove visible&(filtered or out of bounds ) to hidden
-			//jsdump('Moving from visible to hidden: ' + pVisibleMarkers[key].id );
-			pHiddenMarkers[key] = pVisibleMarkers[key];
-			map.removeOverlay(pVisibleMarkers[key]);
-			delete pVisibleMarkers[key];
-		}
-	}
-	try{	
-		for( var key in pHiddenMarkers ){
-			if( that.inBounds(pBounds, pHiddenMarkers[key].YGeoPoint.Lat, pHiddenMarkers[key].YGeoPoint.Lon) ){
-				if( markerMgr.unfilteredMarker(pHiddenMarkers[key], fils) ){
-					if ( visicount < 200 ){
-						//jsdump('Moving from hidden to visible: ' + pHiddenMarkers[key].id );
-						visicount++;
-						pVisibleMarkers[key] = pHiddenMarkers[key];
-						map.addOverlay(pVisibleMarkers[key]);
-						delete pHiddenMarkers[key];	
-					} else {
-						throw 'Map density ceiling';
-				 	}
-				}
-			} else if( pHiddenMarkers[key].YGeoPoint.Lat === 0 && pHiddenMarkers[key].YGeoPoint.Lon === 0 ){
-				// The marker has been created but not yet geocoded. Add it to the map and 
-				// leave it alone until it's been geocoded.
-				visicount++;
-				pVisibleMarkers[key] = pHiddenMarkers[key];
-				map.addOverlay(pVisibleMarkers[key]);
-				delete pHiddenMarkers[key];	
-			}
-		}
-	} catch(e){
-		this.warningPinCeiling(true);
-	}
-	var totalcount = 0;
-	for(var m in pVisibleMarkers){
-		totalcount++;
-	}
-	for(var m in pHiddenMarkers){
-		totalcount++;
-	}
-	$('#feedback_pincounts').text(visicount + ' / ' + totalcount);
-	$('#sel_change_pin_label').trigger( 'change');
-}
 
-DomManager.prototype.drawInitMarkers = function( pPoll, pCallback){
-	/* If pPoll then wait until xscopeNS.flags.loadingData is finished.
-	 */
-	var that = this;
-	if( pPoll){
-		if(xscopeNS.flags.loadingData){
-			setTimeout( that.drawInitMarkers,555, true, pCallback );
-		} else {
-			setTimeout( that.drawInitMarkers,1, false, pCallback );
-		}
-	} else {
-		dataMgr.emptyObj( xscopeNS.pointMarkers );
-		dataMgr.emptyObj( xscopeNS.hiddenMarkers );
-		try{
-			markerMgr.createDomPointMarkers(xscopeNS.KML, xscopeNS.hiddenMarkers );
-		} catch(e){
-			jsdump('Exception:\n'+e);
-		}
-		pCallback.call(that);
-	}
-}
-DomManager.prototype.drawInitPointlessMarkers = function( pPoll, pCallback ){
-	/* Markers without coordinates are plonked onto the map and left there until
-	 * their geocoding comes back.
-	 */
-	var that = this;
-	if( pPoll){
-		if(xscopeNS.flags.loadingData){
-			setTimeout( that.drawInitPointlessMarkers,555, true, pCallback );
-		} else {
-			setTimeout( that.drawInitPointlessMarkers,1, false, pCallback );
-		}
-	} else {
-		dataMgr.emptyObj( xscopeNS.geoMarkers );
-		dataMgr.emptyObj( xscopeNS.errorMarkers );
-		try{
-			markerMgr.createDomGeocodeMarkers(xscopeNS.KML, xscopeNS.geoMarkers );
-		} catch(e){
-			jsdump('Exception:\n'+e);
-		}
-		$.each(xscopeNS.geoMarkers, function(key, mkr){
-			map.addOverlay(xscopeNS.geoMarkers[key]);
-		});
-		pCallback.call(that);
-	}
-}
+
 DomManager.prototype.initPan = function(){
 	// Specifying the Map starting location and zoom level
 	if ( xscopeNS.flags.scrollOnGeocodeSuccess){
@@ -168,91 +281,7 @@ DomManager.prototype.initPan = function(){
 		}
 	}
 }
-DomManager.prototype.drawControls = function( pPoll){
-	var that = this;
-	if( pPoll){
-		if(xscopeNS.flags.loadingData){
-			setTimeout( that.drawControls,555, true );
-		} else {
-			setTimeout( that.drawControls,1, false );
-		}
-	} else {
-		// Filtering stuff
-		var pinTagSets = dataMgr.domTagSetCensus( xscopeNS.KML );
-			
-		var html = '<table id="tbl_filters"></table>';
-		$("#div_filters").empty();
-		$("#div_filters").append(html);
-	
-	 	var filcnt = 0;
-		$.each( pinTagSets, function(tagset, tags){
-			html = '<tr><td id="divFilter_' + filcnt + '"></td></tr>';
-			$('#tbl_filters').append(html);
-			domMgr.drawTagsetFilter('divFilter_' + filcnt, tagset, pinTagSets );
-			filcnt++;
-		});
-		
-		// Attach an event handler to the filter stuff
-		$('.tagset_filter').bind('change', function(e){
-			domMgr.drawMarkers( xscopeNS.pointMarkers, xscopeNS.hiddenMarkers, map.getBoundsLatLon() );
-		});
-		
-		// Pin label stuff
-		var pinItems = dataMgr.domDataCensus( xscopeNS.KML );	
-		var labels = [];
-		for (var pi in pinItems){
-			labels.push(pi);
-		}
-		domMgr.drawLabelSelector('pin_label_selector', labels );
-		$('#sel_change_pin_label').bind( 'change', function(e){
-			markerMgr.setPinLabels( xscopeNS.pointMarkers, this.value );
-		});
-	
-		// Popup Selectors	
-		domMgr.drawPopupSelector('pin_popup_switches', labels );	
-	}
 
-}
-DomManager.prototype.drawLabelSelector = function( pDivId, pLabels ){
-	/* Args: The string id of the div and an array of label options */
-	var dd = $('#' + pDivId);
-	dd.empty();
-	var sel = '<SELECT id="sel_change_pin_label">';
-	$.each( pLabels, function(i,val){
-		sel += '<OPTION>'+val+'</OPTION>\n';
-	});
-	sel += '</SELECT>';
-	if( pLabels.length > 0){
-		dd.append( sel );
-	} else {
-		dd.append('<div class="warn">No data</div>');
-		jsdump('No pin labels found in data');
-	}
-}
-DomManager.prototype.drawPopupSelector= function( pDivId, pLabels){
-	/* Args: The string id of the div and and an array of label options */
-	var dd = $('#' + pDivId);
-	dd.empty();
-	var html = '';	
-	$.each( pLabels, function(i, val){
-		html += '<UL id="ul_popup_selector">';
-		html += '<LI><input class="cb_popup_selector"  type="checkbox" checked="checked" value="'+val+'">'+val+'</LI>';
-		html += '</UL> \n';
-	});
-	if( pLabels.length > 0){
-		dd.append( html );
-	} else {
-		dd.append('<div class="warn">No data</div>');
-	}
-}
-DomManager.prototype.getPopupSelection = function( ){
-	/* returns an array of members that should be shown in popups */
-	var ret = new Array();
-	$('.cb_popup_selector:checked').each(function(i,v){
-		ret.push( $(v).val() );
-	});
-	return ret;
-}
 DomManager.prototype.drawTagsetFilter = function( pDivId, pTag, pTagData){
 	/* Draws a filter for a tagset. Where to draw it is pDivId, which tagset is pTag.
 	 * pTagData is the nested hash as defined in DataManager.getTags.
