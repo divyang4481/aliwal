4ca5c0c6f99310031d34b45f92f539e48b2cddf3
baudmap <baudmap@4e451f21-b44b-0410-a7af-17a7e65364fd>
2008-10-30 11:09:23 +0000

    Bump version number

--- application/defaults/preferences/prefs.js
+++ application/defaults/preferences/prefs.js
@@ -16,7 +16,7 @@
 */
 
 pref("toolkit.defaultChromeURI", "chrome://AliwalGeocoder/content/AliwalGeocoder.xul");
-pref("general.useragent.extra.AliwalGeocoder", "AliwalGeocoder/0.1");
+pref("general.useragent.extra.AliwalGeocoder", "AliwalGeocoder/0.8");
 
 pref("browser.preferences.animateFadeIn", false);
 pref("browser.preferences.instantApply", true);
